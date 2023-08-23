e8087b78757f780693ddb1511a184702f4e76aca
jswan <jswannieman@gmail.com>
2023-08-24 11:37:20 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import { useState, useRef } from "react";
+import ConfirmationModal from "./ConfirmationModal";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const modalRef = useRef();
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <div className="app-btns">
+        <button onClick={() => setOpen((prevSetOpen) => !prevSetOpen)}>
+          Open
+        </button>
+        <button onClick={() => modalRef.current.focusCloseButton()}>
+          Focus Close
+        </button>
+        <button onClick={() => modalRef.current.focusConfirmButton()}>
+          Focus Confirm
+        </button>
+        <button onClick={() => modalRef.current.focusDenyButton()}>
+          Focus Deny
+        </button>
+      </div>
+      <ConfirmationModal
+        ref={modalRef}
+        isOpen={open}
+        onClose={() => setOpen(false)}
+      />
+    </>
   );
 }
 
